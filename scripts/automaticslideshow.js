67e619730c5528b497dafdd10d6f4a9e792684ae
danielthoms <dthoms@cca.edu>
2016-09-03 16:11:27 +0200

    Update automaticslideshow.js

--- scripts/automaticslideshow.js
+++ scripts/automaticslideshow.js
@@ -1,4 +1,4 @@
-var slideIndex = 0;
+var slideIndex = 1;
 showSlides();
 
 function showSlides() {
@@ -7,8 +7,20 @@ function showSlides() {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; 
     }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 7000); // Change image every 7 seconds
+    setTimeout(circleSlides, 7000); // Change image every 7 seconds
+}
+
+function circleSlides(){
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    showSlides();
+    
+}
+
+function plusAuSlides(n)
+  slideIndex+=n;
+  if (slideIndex> slides.length) {slideIndex = 1}
+  if (slideIndex< 1) {slideIndex = slides.length}
+  showSlides(slideIndex += n);
 }
